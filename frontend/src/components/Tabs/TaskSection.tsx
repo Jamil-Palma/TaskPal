027bcb7a29dab15b34d0a1124a59387003e534ae
Jamil-Palma <jb.palma.salazar@gmail.com>
2024-07-18 18:18:03 +0000

    local changes

--- frontend/src/components/Tabs/TaskSection.tsx
+++ frontend/src/components/Tabs/TaskSection.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Box, Grid, Typography } from '@mui/material';
 import TaskCard from './TaskCard';
 import arrow from '../../assets/images/upgrade.png';
+
 interface TaskData {
   title: string;
   description: string;
   imageSrc: string;
   titleBackgroundSrc: string;
+  url: string;
 }
 
 const taskData: TaskData[] = [
@@ -14,25 +16,28 @@ const taskData: TaskData[] = [
     title: "Predefined Tasks",
     description: "Data technology futuristic illustration. A wave of bright particles. Technological 3D landscape. Big data visualization. Network of dots connected by lines. Abstract digital background. 3d rendering 8k, hyperrealistic, dark blue to baby pink, ultra-realistic, high resolution, very detailed, very sharpen",
     imageSrc: "/images/Predefiend_task_compressed.jpg",
-    titleBackgroundSrc: "/images/title_3.png" 
+    titleBackgroundSrc: "/images/title_3.png",
+    url: "/predefined-tasks"
   },
   {
     title: "URL Tasks",
     description: "Data technology futuristic illustration. A wave of bright particles. Technological 3D landscape. Big data visualization. Network of dots connected by lines. Abstract digital background. 3d rendering 8k, hyperrealistic, dark blue to baby pink, ultra-realistic, high resolution, very detailed, very sharpen",
     imageSrc: "/images/URL_Task_compressed.jpg",
-    titleBackgroundSrc: "/images/title_3.png"
+    titleBackgroundSrc: "/images/title_3.png",
+    url: "/url-task"
   },
   {
     title: "Chat",
     description: "Data technology futuristic illustration. A wave of bright particles. Technological 3D landscape. Big data visualization. Network of dots connected by lines. Abstract digital background. 3d rendering 8k, hyperrealistic, dark blue to baby pink, ultra-realistic, high resolution, very detailed, very sharpen",
     imageSrc: "/images/chat_compressed.jpg",
-    titleBackgroundSrc: "/images/title_3.png" 
+    titleBackgroundSrc: "/images/title_3.png",
+    url: "/empty-chat"
   }
 ];
 
 const TaskSection: React.FC = () => {
   return (
-    <Box component="section" py={5} px={2} maxWidth="1200px" mx="auto" >
+    <Box component="section" py={5} px={2} maxWidth="1200px" mx="auto" mt={-50} sx={{ position: 'relative', zIndex: 1 }}>
       <Box
         component="img"
         src={arrow} 
@@ -49,7 +54,8 @@ const TaskSection: React.FC = () => {
               title={task.title}
               description={task.description}
               imageSrc={task.imageSrc}
-              titleBackgroundSrc={task.titleBackgroundSrc} 
+              titleBackgroundSrc={task.titleBackgroundSrc}
+              url={task.url}
             />
           </Grid>
         ))}

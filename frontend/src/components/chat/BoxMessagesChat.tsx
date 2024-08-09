f526a6626aa298cf43703bc41552e2b9bb5c0e1c
eluzung <luzungelaine@gmail.com>
2024-08-09 16:41:21 -0400

    style: change conversation styling in the chat page

--- frontend/src/components/chat/BoxMessagesChat.tsx
+++ frontend/src/components/chat/BoxMessagesChat.tsx
@@ -18,7 +18,7 @@ const BoxMessagesChat: React.FC<BoxMessagesChatProps> = ({ messageList }) => {
   const renderMessageContent = useRenderMessageContent();
 
   return (
-    <Paper sx={{ padding: '16px', marginTop: '16px', maxHeight: '60vh', overflow: 'auto', backgroundColor: 'rgba(28, 28, 28, 0.8)', color: '#ffffff', borderRadius: '8px' }}>
+    <Paper sx={{ padding: '16px', marginTop: '16px', height: '60vh', overflow: 'auto', backgroundColor: 'rgba(28, 28, 28, 0.8)', color: '#ffffff', borderRadius: '8px' }}>
       {messageList.map((message, index) => (
         <Box 
           key={index} 
@@ -27,23 +27,26 @@ const BoxMessagesChat: React.FC<BoxMessagesChatProps> = ({ messageList }) => {
             flexDirection: message.role === 'user' ? 'row-reverse' : 'row', 
             alignItems: 'center', 
             justifyContent: message.role === 'user' ? 'flex-end' : 'flex-start',
+
             mb: 2,
+
           }}
         >
-          <Avatar sx={{ bgcolor: message.role === 'user' ? 'rgba(151, 71, 255, 0.8)' : 'rgba(174, 139, 255, 0.8)', marginLeft: message.role === 'user' ? 2 : 0, marginRight: message.role === 'user' ? 0 : 2 }}>
+          <Avatar sx={{ bgcolor: message.role === 'user' ? 'rgba(255, 241, 115, 0.9)' : 'rgba(174, 139, 255, 0.8)', marginLeft: message.role === 'user' ? 2 : 0, marginRight: message.role === 'user' ? 0 : 2 }}>
             {message.role === 'user' ? <AccountCircleIcon /> : <SmartToyIcon />}
           </Avatar>
           <Box 
             sx={{ 
               border: 1, 
-              borderColor: message.role === 'user' ? 'rgba(151, 71, 255, 0.8)' : 'rgba(174, 139, 255, 0.8)', 
-              borderRadius: 4, 
-              bgcolor: message.role === 'user' ? 'rgba(151, 71, 255, 0.6)' : 'rgba(174, 139, 255, 0.6)', 
+              borderColor: message.role === 'user' ? 'rgba(255, 241, 115, 0.6)' : 'rgba(174, 139, 255, 0.8)', 
+              borderRadius: '15px', 
+              bgcolor: message.role === 'user' ? 'rgba(255, 241, 115, 0.7)' : 'rgba(174, 139, 255, 0.6)', 
               color: '#ffffff',
               boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
               maxWidth: '75%',
               ml: message.role === 'user' ? 'auto' : 0,
               mr: message.role === 'user' ? 0 : 'auto',
+              p: 1.5
             }}
           >
             {renderMessageContent(message.content)}
